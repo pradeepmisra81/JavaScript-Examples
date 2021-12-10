45e398bac233d66e5c209c0f825051020fc85835
pradeep <pradeep.misra@gmail.com>
2021-12-10 20:02:09 +0530

    Chap2: namespace or containership learning

---
+++ Chap2/namespaceFaking.js
@@ -0,0 +1,27 @@
+var greet = 'Namaskar';
+var greet = 'Hello';
+
+
+console.log(greet);
+
+var hindi = {};
+var english = {};
+var spanish = {};
+
+hindi.greet = 'namaskar';
+english.greet = 'hello';
+
+console.log(hindi);
+console.log(english);
+
+
+console.log(hindi.greet);
+console.log(english.greet);
+
+spanish.greetings = {};
+
+spanish.greetings.greet = 'Holla!';
+
+console.log(spanish);
+console.log(spanish.greetings);
+console.log(spanish.greetings.greet);
