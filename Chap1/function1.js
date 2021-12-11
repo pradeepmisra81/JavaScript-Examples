00aa0289636d651d1d53c456b9b9acc414b34b22
pradeep <pradeep.misra@gmail.com>
2021-12-11 12:29:09 +0530

    Chap1: function expression & statment learning

---
+++ Chap1/function1.js
@@ -0,0 +1,9 @@
+function swagat() {
+    console.log('Aapka swagat hai JavaScript ki dunia me!');
+}
+
+
+swagat.language = 'hindi';
+
+swagat();
+console.log(swagat.language);
---
+++ Chap1/functionExpStatement.js
@@ -0,0 +1,12 @@
+welcome();
+
+function welcome() {
+    console.log('Welcome to JavaScript World!');
+}
+
+
+var anonymousWelcome = function() {
+    console.log('Welcome to anonymous World!');
+}
+
+anonymousWelcome();
