7443e20dd465454450e563a2cf13159ffd65ac57
pradeep <pradeep.misra@gmail.com>
2022-01-05 20:34:05 +0530

    Chap1: Function arguments learning

---
+++ Chap1/ArgumentsSpread/argumentsSpread.js
@@ -0,0 +1,11 @@
+function swagat(firstname, lastname, language) {
+    console.log(firstname);
+    console.log(lastname);
+    console.log(language);
+    console.log("_____________________________________");
+}
+
+swagat();
+swagat("Ram");
+swagat("Ram", "Kumar");
+swagat("Ram", "Kumar","en");
